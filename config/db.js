1edf335eeb882a990a5190b7ce9acea15e0a89e5
J1Loop <jorge.ua.1996@gmail.com>
2022-07-11 17:42:15 +0200

    commit fixed

--- config/db.js
+++ config/db.js
@@ -2,5 +2,3 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(process.env.MONGO_URL);
-
-// mongodb+srv://jorge_admin:jorge123@chat-database.sv3reaj.mongodb.net/?retryWrites=true&w=majority
